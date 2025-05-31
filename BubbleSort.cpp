b5ef05a47224f8920f0516909ab3b4303df13259
wasimasultana230 <wasimasultana230@gmail.com>
2025-05-31 09:00:53 +0600

    Bubble Sort in ascending order

---
+++ BubbleSort.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std ;
+int main (){
+cout<< "Enter array size : ";
+    int n;
+    cin >> n;
+    int array[n];
+    cout << "Enter the elements of the array : ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> array[i];
+    }
+    // bubblr sort in ascending order 
+    int counter = 1;
+    while(counter < n){
+        for (int i = 0 ; i < n-counter ; i++){
+            if(array[i] > array[i+1]){
+            int temp = array[i];
+            array[i] = array[i+1];
+            array[i+1] = temp;
+            }           
+        }
+        counter++;
+    } 
+    cout<<"Sort in ascending order : \n";
+    for ( int i = 0; i < n; i++){
+        cout << array[i] <<" ";
+    }
+
+    return 0;
+}
